9f761fc3e6e64445632d9ddac2182d874c9ee798
Vaghasiya Vikas <vikas@socialpilot.co>
2020-05-03 10:45:16 +0530

    -- add alert tip

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,7 +3,7 @@ import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
-import BlogPostPreview from './preview-templates/BlogPostPreview'
+// import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 import FeaturePagePreview from './preview-templates/FeaturePagePreview'
@@ -28,4 +28,4 @@ CMS.registerPreviewTemplate('categories', CategoryPagePreview)
 CMS.registerPreviewTemplate('topics', TopicsPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('blog', BlogPostPreview)
+// CMS.registerPreviewTemplate('blog', BlogPostPreview)
